9467fa4bcff60059794e17a877ee92a331da1409
Navroop Singh Bajwa <navroop.bajwa@yahoo.com>
2017-09-04 10:33:22 +0530

    Initial Commit

---
+++ LaplaceTransform/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LaplaceTransform
+//
+//  Created by Navroop Singh Bajwa on 04/09/17.
+//  Copyright © 2017 Bajwa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
